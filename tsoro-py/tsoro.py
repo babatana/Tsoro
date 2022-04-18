f9cd10ae4948aa05530bf5ca352cd4c431bae2ac
Tanaka  Khondowe <t.khondowe@gmail.coml>
2022-04-18 00:47:06 -0500

    Initialized board as a atrix

--- tsoro-py/tsoro.py
+++ tsoro-py/tsoro.py
@@ -1,1 +1,17 @@
-print("hello world")
+### Python Tsoro Game Tree
+### Includes DFS, BFS and Djikstra Algorithms 
+### Written by Tanaka B. Khondowe
+
+
+A = [
+    [1, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+boardA = len(A[0])
+boardB = len(A[1])
+
+i = 0
+while i < boardA:
+    print(A[0][i])
+    i+=1
