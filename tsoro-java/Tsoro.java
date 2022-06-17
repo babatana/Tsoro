a47d306bf6cf856d1e81b5b0e0eeafb563b98934
Tanaka <t.khondowe@gmail.com>
2022-06-16 23:07:39 -0500

    New Tsoro implementation

--- tsoro-java/Tsoro.java
+++ tsoro-java/Tsoro.java
@@ -1,18 +1,18 @@
-import java.util.*; 
+import java.util.*;
 
-public class Tsoro {
+public class Tsoro{
   ArrayList<Integer> board;
-  int hand;	
-  int currHole;
-  int nextHole;
-  int destHole;
 
-  public Tsoro(ArrayList<Integer> initBoard){
-    board = initBoard;
-    hand = 0;
-    currHole = 0;
-    nextHole = currHole+1;;
-    destHole = 0;
+  public Tsoro() {
+    Integer currHole = 0; 
+    Integer destHole = 0; 
+    Integer hand = 0;
+
+  }
+
+  public ArrayList<Integer> initboard(Integer boardSize, Integer stonesPerhole) {
+    
+    return board;
   }
 
   public void showBoard() {
@@ -33,57 +33,6 @@ public class Tsoro {
     }
     System.out.println("]");
   }
-  
-  protected void checkIfOutOfIndex() {
-    if (currHole >= board.size() || nextHole >= board.size()) {
-      currHole -= board.size();
-      nextHole -= board.size();
-    }
-  } 
 
-  protected ArrayList<Integer> makeMove (int handIndex) {
-    System.out.println("Move");
 
-    if (board.get(handIndex) > 0) {
-      hand = board.get(handIndex);      
-      board.set(handIndex, 0);
-      currHole = handIndex+1;
-    } else {
-      System.out.println("You selected an empty hand, hole " + currHole);
-    }
-    
-    while (hand > 0) {
-      // avoid out-of-range error 
-      if (currHole >= board.size()) {
-        currHole -= board.size();
-      }
- 
-      if (hand == 1 && board.get(currHole) == 0) {
-        int emptyHoleTracker = 0;
-        while (board.get(nextHole) == 0) {
-          if (currHole >= board.size() || nextHole >= board.size()) {
-            currHole -= board.size();
-            nextHole -= board.size();
-          }
-          currHole++;
-          nextHole++;
-          if (currHole >= board.size() || nextHole >= board.size()) {
-            currHole -= board.size();
-            nextHole -= board.size();
-          }
-        }
-      }
-
-      if (hand == 1 && currHole == board.size()Z) {
-        System.out.println("Move complete!");
-        return board;
-      }
-      board.set(currHole, board.get(currHole)+1);
-      hand -= 1;
-      currHole++; 	
-    }
-    showBoard();
-    return board;
-  }
 }
-
