25aaa003a00038ff627ca3262f23cf81295c6b56
Tanaka <t.khondowe@gmail.com>
2022-07-24 22:17:42 -0500

    learning igraph

---
+++ tsoro-py/igraph-example.py
@@ -0,0 +1,20 @@
+# https://igraph.org/python/tutorial/latest/tutorial.html
+
+import igraph
+print(igraph.__version__)
+
+from igraph import *
+
+g = Graph()
+
+print(g)
+g.add_vertices(3)
+print(g)
+g.add_edges([(0,1), (1,2)])
+print(g)
+g.add_edges([(2, 0)])
+print(g)
+g.add_vertices(3)
+print(g)
+g.add_edges([(2, 3), (3, 4), (4, 5), (5, 3)])
+print(g)
