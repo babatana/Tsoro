b9989ad5b6156f454fe5f7fa46aa95f61420b8ec
Tanaka <t.khondowe@gmail.com>
2022-08-02 23:58:55 -0500

    Setting and retrieving attributes for graphs

--- tsoro-py/igraph-example.py
+++ tsoro-py/igraph-example.py
@@ -22,6 +22,24 @@ summary(g)
 g.get_eid(2, 3)
 
 
-# deterministic graph generated
+# deterministic graph generated, a regular tree graph
+# 127 vertices each with two children
 g2 = Graph.Tree(127, 2)
+
+# get the first 10 edges
+g2.get_edgelist()[0:10]
+
+
+# stochastic generator
+g = Graph.GRG(100, 0.2)
+
+# Setting and retrieving attributes
+# use **vs** or **es** as a dictionary, you are assigning attributes to all vertices/edges of the graph
+# vs is the vertices attribute
+# es is the edge attribute
+g.vs
+g.vs["name"] = ["Alice", "Bob", "Claire", "Dennis", "Esther", "Frank", "George"]
+g.vs["age"] = [25, 31, 18, 47, 22, 23, 50]
+g.vs["gender"] = ["f", "m", "f", "m", "f", "m", "m"]
+g.es["is_formal"] = [False, False, True, True, True, False, True, False, False]
 
